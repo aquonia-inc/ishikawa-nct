d31383caee224142a58164c8b2228a5624c4f299
tysmr <t.yoshimura0902+github@gmail.com>
2022-06-06 14:10:16 +0900

    fix code

--- AllInOne_SORACOM/AllInOne_SORACOM.ino
+++ AllInOne_SORACOM/AllInOne_SORACOM.ino
@@ -3,26 +3,34 @@
 
 // Wifi settings
 #include <WiFi.h>
-const char ssid[] = "***";
-const char pass[] = "***";
- 
+#include <WireGuard-ESP32.h>
+#include <HTTPClient.h>
+
+// WiFi configuration --- UPDATE this configuration for your WiFi AP
+char ssid[] = "******";
+char password[] = "******";
+
+// WireGuard configuration --- UPDATE this configuration from JSON
+char private_key[] = "***";  // [Interface] PrivateKey
+IPAddress local_ip(10,253,247,83);            // [Interface] Address
+char public_key[] = "***";     // [Peer] PublicKey
+char endpoint_address[] = "***";    // [Peer] Endpoint
+int endpoint_port = 11010;              // [Peer] Endpoint
+static constexpr const uint32_t UPDATE_INTERVAL_MS = 5000;
+static WireGuard wg;
+static HTTPClient httpClient;
+
 // M5Unit OLED settings
 #include <M5UnitOLED.h>
 M5UnitOLED display;
 M5Canvas canvas(&display);
 
-// Thingsboard settings
-#include <ThingsBoard.h>
-#define TOKEN               "NITDEVICE00" // "ESP32TEST1CORE2"
-#define THINGSBOARD_SERVER  "thingsboard.cloud"   // ThingsBoard server
-WiFiClient espClient;   // using ESP32 for MQTT client
-ThingsBoard tb(espClient);  // init instance
-bool subscribed = false;
-
-#define GROVE_C_1 25
-#define GROVE_C_2 26
-#define GROVE_D_1 35 // analog in
-#define GROVE_D_2 34 // analog in
+// LED Button settings
+#define LEDbuttonLEDPin 27
+#define LEDbuttonButtonPin 4
+int LEDbuttonStatus = LOW;
+unsigned long pushTimebuffer = 500;      //change accordingly
+unsigned long LEDbutton_toggleTime = 0;
 
 // AquoWare Multipurpose Module settings
 #include "multipurpose_module.h"
@@ -35,10 +43,6 @@ float tdsVoltage = 0;
 // pH Sensor settings
 float phValue = 0;
 float phVoltage = 0;
-
-// M5Unit Button settings
-int lastState = LOW;
-int currentState;
 
 // M5Unit EnvⅠⅠ & EnvⅠⅠⅠ settings
 #include <SHT3x.h>
@@ -66,7 +70,7 @@ float lng=136.881537;
 // Water Temperature settings
 #include <OneWire.h>
 #include <DallasTemperature.h>
-#define ONE_WIRE_BUS 32
+#define ONE_WIRE_BUS 33
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 float waterTemp = 0;
@@ -76,33 +80,42 @@ void setup(void) {
   Serial.begin(115200);
   MM.init();
   ss.begin(9600, SERIAL_8N1, RXD2, TXD2);
-  Wire.begin();               // I2Cを初期化する
-  // if (!bme.begin(0x76)) {  // BMP280を初期化する
-  //   Serial.println("BMP280 init fail");
-  // }
+  Wire.begin(); 
   qmp.init(); 
   sht30.Begin();
   sensors.begin();
 
-  pinMode(GROVE_C_1, INPUT);
+  pinMode(LEDbuttonButtonPin, INPUT);
+  pinMode(LEDbuttonLEDPin, OUTPUT);
+  digitalWrite(LEDbuttonLEDPin, LEDbuttonStatus);
 
   display.init();
   display.setRotation(0);
-  canvas.setColorDepth(1); // mono color
-  // canvas.setFont(&fonts::lgfxJapanMinchoP_32);
+  canvas.setColorDepth(1);
   canvas.setTextWrap(false);
   canvas.setTextSize(1.2);
   canvas.setTextScroll(false);
   canvas.createSprite(display.width() , display.height());
   
   WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, pass);
+  WiFi.begin(ssid, password);
   if (WiFi.waitForConnectResult() != WL_CONNECTED) {
     Serial.println("WiFi Failed");
     while(1) {
         delay(1000);
     }
   }
+  Serial.println("Adjusting system time...");
+  configTime(9 * 60 * 60, 0, "ntp.jst.mfeed.ad.jp", "ntp.nict.jp", "time.google.com");
+
+  Serial.println("Connected. Initializing WireGuard...");
+  wg.begin(
+    local_ip,
+    private_key,
+    endpoint_address,
+    public_key,
+    endpoint_port
+  );
 }
 
 void loop(void) {
@@ -118,17 +131,12 @@ void loop(void) {
   sht30.UpdateData();
   tmp = sht30.GetTemperature();
   hum = sht30.GetRelHumidity();
-  //EnvⅠⅠ  
-  //pressure = bme.readPressure();
-  //EnvⅠⅠⅠ
   pressure = qmp.calcPressure();
 
   // water temperature
   sensors.requestTemperatures();
   waterTemp = sensors.getTempCByIndex(0);
 
-  currentState = digitalRead(GROVE_C_1);
-    
   Serial.printf("temp: %4.1f'C\r\n", tmp);
   Serial.printf("humid:%4.1f%%\r\n", hum);
   Serial.printf("press:%4.0fhPa\r\n", pressure / 100);
@@ -141,21 +149,10 @@ void loop(void) {
     Serial.println("Connecting to AP ...");
     Serial.print("Attempting to connect to WPA SSID: ");
     Serial.println(WIFI_AP);
-    WiFi.begin(ssid,pass);
+    WiFi.begin(ssid,password);
     return;
   }
-  // connect to ThingsBoard server
-  if (!tb.connected()) {
-    subscribed = false;
-    Serial.print("Connecting to: ");
-    Serial.print(THINGSBOARD_SERVER);
-    Serial.print(" with token ");
-    Serial.println(TOKEN);
-    if (!tb.connect(THINGSBOARD_SERVER, TOKEN)) {
-      Serial.println("Failed to connect");
-      return;
-    }
-  }
+  
   while (ss.available()) {
     gps.encode(ss.read());
     lat=gps.location.lat();
@@ -163,19 +160,73 @@ void loop(void) {
     // Serial.println(gps.location.lat(),6);
     // Serial.println(gps.location.lng(),6);
   }
-  if (lastState == HIGH && currentState == LOW) {
+
+   
+  WiFiClient client;
+
+  if( !client.connect("uni.soracom.io", 80) ) {
+    Serial.println("Failed to connect...");
+    delay(5000);
+    return;
+  }
+
+  uint64_t uptime_msec = millis();
+  Serial.printf("Sending uptime %lu [ms]\r\n", uptime_msec);
+  String json;
+  json += "{\"pH\":";
+  json.concat(phValue); //json.concat(static_cast<unsigned long>(phValue));
+  json += ",\"TDS\":";
+  json.concat(tdsValue);
+  json += ",\"Tempreture\":";
+  json.concat(tmp);
+  json += ",\"WaterTempreture\":";
+  json.concat(waterTemp);
+  json += ",\"Humidity\":";
+  json.concat(hum);
+  json += ",\"AirPressure\":";
+  json.concat(pressure);
+  json += ",\"Latitude\":";
+  json.concat(lat);
+  json += ",\"Longitude\":";
+  json.concat(lng);
+  json += "}";
+  Serial.printf("payload: %s\r\n", json.c_str());
+  
+  
+  delay(UPDATE_INTERVAL_MS);
+
+  int reading = digitalRead(LEDbuttonButtonPin);
+  
+  if(reading==LOW && millis()-LEDbutton_toggleTime>pushTimebuffer){
+    LEDbuttonStatus = !LEDbuttonStatus;
+    LEDbutton_toggleTime = millis();
     Serial.println("The button is pressed");
-    tb.sendTelemetryFloat("TDS", tdsValue); 
-    //tb.sendTelemetryFloat("pH", phValue); 
-    tb.sendTelemetryFloat("Temperature", tmp);
-    tb.sendTelemetryFloat("Humidity", hum);
-    tb.sendTelemetryFloat("AirPressure", pressure/100);
-    tb.sendTelemetryFloat("WaterTemperature", waterTemp); 
-    tb.sendTelemetryFloat("Latitude", lat);
-    tb.sendTelemetryFloat("Longitude", lng);
-    
+    client.write("POST / HTTP/1.1\r\n");
+    client.write("Host: harvest.soracom.io\r\n");
+    client.write("Connection: Keep-Alive\r\n");
+    client.write("Keep-Alive: timeout=5, max=2\r\n");
+    client.write("Content-Type: application/json\r\n");
+    client.write("Content-Length: ");
+    client.write(String(json.length(), 10).c_str());
+    client.write("\r\n\r\n");
+    client.write(json.c_str());
+
+    while(client.connected()) {
+      auto line = client.readStringUntil('\n');
+      Serial.write(line.c_str());
+      Serial.write("\n");
+      if( line == "\r" ) break;
+    }
+    if(client.connected()) {
+      uint8_t buffer[256];
+      size_t bytesToRead = 0;
+      while((bytesToRead = client.available()) > 0) {
+        bytesToRead = bytesToRead > sizeof(buffer) ? sizeof(buffer) : bytesToRead;
+        auto bytesRead = client.readBytes(buffer, bytesToRead); 
+        Serial.write(buffer, bytesRead);
+      }
+    }
     canvas.pushSprite(0, 5);
-    
     // TDS
     canvas.setCursor(0, 0); 
     canvas.print("TDS");
@@ -187,7 +238,7 @@ void loop(void) {
     canvas.setCursor(0, 20); 
     canvas.print("pH");
     canvas.setCursor(15, 30);
-    canvas.print(String(phValue));
+    canvas.print(String(int(round(phValue))));
 
     // Temperature
     canvas.setCursor(0, 40);
@@ -218,15 +269,9 @@ void loop(void) {
     canvas.print("hPa");
     
   }
-  else if (lastState == LOW && currentState == HIGH){
-    Serial.println("The button is released");
+  if(reading==HIGH) {
+    digitalWrite(LEDbuttonLEDPin, LOW);
   }
-
-  // save the the last state
-  lastState = currentState;
-
-  // event loop for MQTT client
-  tb.loop(); 
 
   //Serial
   Serial.print("tdsVoltage : "); 
